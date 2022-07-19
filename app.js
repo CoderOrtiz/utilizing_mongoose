5be2fad87382d657732efc45a073bf0b59f32975
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-07-19 19:53:24 -0400

    Practicing Updating and Deleting Data using mongoose

--- app.js
+++ app.js
@@ -3,18 +3,42 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fruitDB");
 
 const fruitSchema = new mongoose.Schema ({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, "Please check your data entry, no name specified!"]
+    },
+    // This is mongoose Validation
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit ({
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid as a fruit"
+    name: "Peach",
+    rating: 10,
+    review: "Peaches are yummy"
 });
+
+// Fruit.deleteOne({name: "Peaches"}, function(err){
+//     if (err){
+//         console.log(err);
+//     } else {
+//         console.log(Fruit);
+//     }
+// });
+
+// Fruit.updateOne({_id: "62d73e3387845449a5553b0b"}, {name: "Peach"}, function(err){
+//     if (err){
+//         console.log(err);
+//     } else {
+//         console.log(fruit.name);
+//     }
+// });
 
 const personSchema = new mongoose.Schema ({
     name: String,
@@ -28,39 +52,47 @@ const person = new Person ({
     age: 37
 });
 
-// More Fruits
-const kiwi = new Fruit({
-    name: "Kiwi",
-    score: 10,
-    review: "The best fruit"
-});
+// Person.deleteMany({name: "John"}, function(err){
+//     if (err){
+//         console.log(err);
+//     } else {
+//         console.log("Successfully deleted all the document");
+//     }
+// });
 
-const orange = new Fruit ({
-    name: "Orange",
-    score: 4,
-    review: "Too sour for me"
-});
+// // More Fruits
+// const kiwi = new Fruit({
+//     name: "Kiwi",
+//     score: 1Many0,
+//     review: "The best fruit"
+// });
 
-const banana = new Fruit({
-    name: "Banana",
-    score: 3,
-    review: "Weird Tecture"
-});
+// const orange = new Fruit ({
+//     name: "Orange",
+//     score: 4,
+//     review: "Too sour for me"
+// });
+
+// const banana = new Fruit({
+//     name: "Banana",
+//     score: 3,
+//     review: "Weird Tecture"
+// });
 
 
-Fruit.find(function(err, fruits){
-    if (err) {
-        console.log(err);
-    } else {
+// Fruit.find(function(err, fruits){
+//     if (err) {
+//         console.log(err);
+//     } else {
 
-        // Closes mongoose / kills node in the terminal
-        mongoose.connection.close()
+//         // Closes mongoose / kills node in the terminal
+//         mongoose.connection.close()
 
-        fruits.forEach(function(fruit) {
-            console.log(fruit.name);
-        });
-    }
-});
+//         fruits.forEach(function(fruit) {
+//             console.log(fruit.name);
+//         });
+//     }
+// });
 
 
 
